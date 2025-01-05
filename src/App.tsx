9909f26a04dc14ee6320a805ac1e88b1fddb6678
e30b <eldhobaby40@gmail.com>
2025-01-06 00:20:31 +0530

    Task Done

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { NewTodoForm } from "./newtodoform";
+import "./App.css";
+import { TodoList } from "./todolist";
+import { TodoItemProps } from "./todoitem";
+
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS");
+    if (localValue == null) return [];
+
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title: string) {
+    setTodos((currentTodos: Array<TodoItemProps>) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ];
+    });
+  }
+
+  function toggleTodo(id: string, completed: boolean) {
+    setTodos((currentTodos: Array<TodoItemProps>) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id: string) {
+    setTodos((currentTodos: Array<TodoItemProps>) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
+}
---
+++ src/todoitem.tsx
@@ -0,0 +1,35 @@
+export type TodoItemProps = {
+  id: string;
+  title: string;
+  completed: boolean;
+};
+
+export function TodoItem({
+  completed,
+  id,
+  title,
+  toggleTodo,
+  deleteTodo,
+}: {
+  completed: boolean;
+  id: string;
+  title: string;
+  toggleTodo: (id: string, completed: boolean) => void;
+  deleteTodo: (id: string) => void;
+}) {
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        Delete
+      </button>
+    </li>
+  );
+}
